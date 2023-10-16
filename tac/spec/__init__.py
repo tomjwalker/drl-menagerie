366599f165ad312fc387253b2b37bdfda9e511f7
Tom Walker <37417736+tomjwalker@users.noreply.github.com>
2023-10-16 11:43:14 +0100

    New temporary spec format£

--- tac/spec/__init__.py
+++ tac/spec/__init__.py
@@ -1,1 +1,15 @@
 from .spec_utils import analyse_spec
+
+# TODO: This will need renaming and replacing when final spec solution is ready
+from .reinforce.temp_spec import spec as reinforce_spec
+from .sarsa.temp_spec import spec as sarsa_spec
+
+
+ALGORITHM_SPECS = {
+    "reinforce": reinforce_spec,
+    "sarsa": sarsa_spec,
+}
+
+
+def load_spec(algorithm_name):
+    return ALGORITHM_SPECS[algorithm_name]
---
+++ tac/spec/reinforce/temp_spec.py
@@ -0,0 +1,18 @@
+spec = {
+    "algorithm": "reinforce",
+    "gamma": 0.99,
+    "hidden_layer_units": [64],
+    "learning_rate": 0.01,
+    "environment": "CartPole-v1",
+    "training_episodes": 20,
+    "activation": "relu",
+    "optimiser": "adam",
+    # "training_record_episodes": [0, 100, 499],
+    "data_directory": ".data",
+    "num_sessions": 2,
+    "num_trials": 2,
+    # "search": {
+    #     "learning_rate__choice": [0.01, 0.001],
+    #     "gamma__uniform": [0.5, 1.0],
+    # }
+}
