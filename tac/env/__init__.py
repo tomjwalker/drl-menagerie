0f2dd5fe0227dc54f6a58475fc48a3f4e168ad4a
Tom Walker <37417736+tomjwalker@users.noreply.github.com>
2023-10-30 19:32:49 +0000

    make_env API method for instantiating environments

--- tac/env/__init__.py
+++ tac/env/__init__.py
@@ -2,5 +2,6 @@ from tac.env.openai import OpenAIEnv
 
 
 def make_env(spec):
+    """API for creating an environment from a spec within the TAC framework"""
     env = OpenAIEnv(spec)
     return env
